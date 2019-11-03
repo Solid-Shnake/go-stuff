9f63219cc71ebfd7e4cf31eeb6a4cddc1cd9da85
joeyscannell <joeyscannell@gmail.com>
2019-11-03 22:25:43 +0000

    url-fetch retreives page sizes and time taken to download

---
+++ url-fetch/fetch.go
@@ -0,0 +1,39 @@
+// this fetches URLs in parrallel and reoprts their times and sizes.package fetch
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a go routine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint() // send the channel ch
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("While reading: %s: %v", url, err) // send the channel ch
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs	%7d    %s", secs, nbytes, url)
+}
